7d75305758f8e630beb2ff80a5590d4855ee28df
itcast <itheima@itcast.cn>
2020-11-30 14:54:33 +0800

    t commit

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ import Register from '@/page/register.vue'
 Vue.use(Router)
 
 export default new Router({
-    mode: 'history',
+    // mode: 'history',
     routes: [{
             path: '/',
             name: 'defaultPage',
